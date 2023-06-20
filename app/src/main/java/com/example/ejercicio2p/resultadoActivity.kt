1288551d30f8978b645fe555e2bf49edaa6e1c49
Jokas-null <jokasblue@gmail.com>
2023-06-20 17:43:19 -0600

    funcionalidad main activity

---
+++ app/src/main/java/com/example/ejercicio2p/resultadoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ejercicio2p
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class resultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
